4e9041f0e1158df8e05412748739582a85839e20
Vesakar <pvvesakar@gmail.com>
2024-10-16 11:39:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,18 @@ const dotenv = require("dotenv")
 const path = require("path")
 const cors = require("cors")
 dotenv.config({path: path.join(__dirname,'config','.env')})
-const PORT = process.env.PORT
+const PORT = process.env.PORT  || 5000
 const connectDB = require('./config/connectDatabase')
 connectDB()
 
 app.use(express.json())
 
 app.use(cors())
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const productRoutes = require('./routes/productRoutes')
 const orderRoutes = require('./routes/orderRoutes')
